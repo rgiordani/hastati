4b2fa0aa2c96979507f112eba1aba1f0c7778dda
Riccardo Giordani <riccardo.giordani@gmail.com>
2024-05-01 00:57:47 +0200

    Implementation of alignCursors command

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 
 	// Aligns the text at the cursors to the rightmost cursor column
-	let disposable = vscode.commands.registerCommand('hastati.alignText', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('hastati.alignText', () => {
 		const editor = vscode.window.activeTextEditor;
 		if (editor === undefined) { return; }
 
@@ -16,9 +16,19 @@ export function activate(context: vscode.ExtensionContext) {
 				editBuilder.insert(sel.active, " ".repeat(numSpaces));
 			}
 		}));
-	});
+	}));
 
-	context.subscriptions.push(disposable);
+	// Aligns the cursors to the leftmost cursor column
+	context.subscriptions.push(vscode.commands.registerCommand('hastati.alignCursors', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor === undefined) { return; }
+
+		const minColumn = Math.min(...editor.selections.map((sel) => sel.active.character));
+
+		editor.selections = editor.selections.map((sel) =>
+			new vscode.Selection(sel.active.line, minColumn, sel.active.line, minColumn)
+	    );
+	}));
 }
 
 export function deactivate() { }
